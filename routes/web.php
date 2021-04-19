4d5807b0a78415bc87e2742d903c16326f50e622
Marcio Osshiro <marcio.osshiro@ifms.edu.br>
2021-04-19 09:51:39 -0400

    19 de abril

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\CategoriaController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,3 +17,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/categoria', [CategoriaController::class, 'index']);
